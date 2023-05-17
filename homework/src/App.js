8e6fd2a2813713821d9d14f1f043e4a70857e023
Ilya Petrenko <ilya.petrenko.96@mail.ru>
2023-05-17 19:37:52 +0300

    homework_2

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { MyComponent } from './MyComponent';
 
 export const App = () => {
 	return (
@@ -17,6 +18,7 @@ export const App = () => {
 				>
 					Learn React
 				</a>
+				<MyComponent type="normal" value="123" />
 			</header>
 		</div>
 	);
---
+++ homework/src/MyComponent.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+export const MyComponent = (props) => {
+	const date = new Date();
+
+	useEffect(() => {
+		console.log(date);
+	}, []);
+
+	return <div>{String(date)}</div>;
+};
